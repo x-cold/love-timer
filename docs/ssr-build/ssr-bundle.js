6d71b3e0bc7c02acf89456d870d1c766d327fedf
x-cold <lxstart@outlook.com>
2017-12-20 23:57:47 +0800

    chore: build test

--- docs/ssr-build/ssr-bundle.js
+++ docs/ssr-build/ssr-bundle.js
@@ -58,7 +58,7 @@ module.exports =
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/2years/";
+/******/ 	__webpack_require__.p = "./docs/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = "JkW7");
--- preact.config.js
+++ preact.config.js
@@ -6,6 +6,16 @@
  * @param {object} env - options passed to CLI.
  * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
  **/
+
+const path = require('path');
+
 export default function (config, env, helpers) {
   /** you can change config here **/
+  if (config.output.path.indexOf('ssr') > -1) {
+    // config.output.path = path.join(__dirname, '/docs/ssr-build');
+  } else {
+    config.output.path = path.join(__dirname, '/docs/');    
+  }
+  console.log(config.output);
+  config.output.publicPath = '/2years/';
 }
