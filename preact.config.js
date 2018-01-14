d92e8813bf0169e57283d6695ef13f943d387be7
x-cold <lxstart@outlook.com>
2018-01-14 19:51:32 +0800

    chore: 废弃 github pages

--- preact.config.js
+++ preact.config.js
@@ -10,14 +10,5 @@
 const path = require('path');
 
 export default function (config, env, helpers) {
-  if (env._.indexOf('watch') > -1) {
-    return;
-  }
-  config.output.publicPath = '/2years/';
-  /** you can change config here **/
-  if (config.output.path.indexOf('ssr') > -1) {
-    config.output.path = path.join(__dirname, '/docs/ssr-build');
-  } else {
-    config.output.path = path.join(__dirname, '/docs/');    
-  }
+  
 }
